0478424caf76ff96bfcac840b6f26a32ccfe98db
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-08-23 19:01:40 +0530

    Update Home.js

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -42,7 +42,7 @@ const Home = () => {
           <h2>Inventory Management Solution</h2>
           <p>
             Inventory system to control and manage products in the warehouse in
-            real time and integrated to make it easier to develop your business.
+            real time and integrated to make it easier to develop your business.<br />
           </p>
           <div className="hero-buttons">
             <button className="--btn --btn-secondary">
@@ -50,7 +50,7 @@ const Home = () => {
             </button>
           </div>
           <div className="--flex-start">
-            <NumberText num="14K" text="Brand Owners" />
+            <NumberText num="42K" text="Brand Owners" />
             <NumberText num="23K" text="Active Users" />
             <NumberText num="500+" text="Partners" />
           </div>
