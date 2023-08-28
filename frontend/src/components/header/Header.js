78631845b14ba5076832eca2c0c065fa45eb3b17
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-08-28 17:53:33 +0530

    Add files via upload

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,63 @@
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { selectName, SET_LOGIN } from "../../redux/features/auth/authSlice";
+import { logoutUser } from "../../services/authService";
+import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
+import { SET_NAME, SET_USER } from "../../redux/features/auth/authSlice";
+import { getUser } from "../../services/authService";
+import Card from "../../components/card/Card";
+import "./Header.scss"
+
+const Header = () => {
+  useRedirectLoggedOutUser("/login");
+  const dispatch = useDispatch();
+
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    console.log("Getting use");
+    setIsLoading(true);
+    async function getUserData() {
+      const data = await getUser();
+      console.log(data);
+
+      setProfile(data);
+      setIsLoading(false);
+      await dispatch(SET_USER(data));
+      await dispatch(SET_NAME(data.name));
+    }
+    getUserData();
+  }, [dispatch]);
+  const navigate = useNavigate();
+  const name = useSelector(selectName);
+
+  const logout = async () => {
+    await logoutUser();
+    await dispatch(SET_LOGIN(false));
+    navigate("/login");
+  };
+
+  return (
+    <div className="--pad header">
+      <div className="--flex-between">
+        {/* <Card cardClass={"card --flex-dir-column"}>
+          <span className="profile-photo">
+            <img src={profile?.photo} alt="profilepic" />
+          </span>
+        </Card> */}
+        <h3>
+          <span className="--fw-thin">Welcome, </span>
+          <span className="--color-danger">{name}</span>
+        </h3>
+        <button onClick={logout} className="--btn --btn-danger">
+          Logout
+        </button>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default Header;
