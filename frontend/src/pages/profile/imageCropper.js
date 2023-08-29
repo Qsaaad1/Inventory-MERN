8815b2855f851cdae107a6d4f7cc585c6f700400
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-08-29 16:20:02 +0530

    Update imageCropper.js

--- frontend/src/pages/profile/imageCropper.js
+++ frontend/src/pages/profile/imageCropper.js
@@ -5,15 +5,15 @@ function ImageCropper({ image, onCropDone, onCropCancel }) {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
   const [croppedArea, setCroppedArea] = useState(null);
-  const [aspectRatio, setAspectRatio] = useState(4 / 3);
+  const [aspectRatio, setAspectRatio] = useState(1 / 1);
 
   const onCropComplete = (croppedAreaPercentage, croppedAreaPixels) => {
     setCroppedArea(croppedAreaPixels);
   };
 
-  const onAspectRatioChange = (event) => {
-    setAspectRatio(event.target.value);
-  };
+  // const onAspectRatioChange = (event) => {
+  //   setAspectRatio(event.target.value);
+  // };
 
   return (
     <div className="cropper">
@@ -28,8 +28,8 @@ function ImageCropper({ image, onCropDone, onCropCancel }) {
           onCropComplete={onCropComplete}
           style={{
             containerStyle: {
-              width: "100%",
-              height: "80%",
+              width: "50%",
+              height: "50%",
               backgroundColor: "#fff",
             },
           }}
@@ -37,7 +37,7 @@ function ImageCropper({ image, onCropDone, onCropCancel }) {
       </div>
 
       <div className="action-btns">
-        <div className="aspect-ratios" onChange={onAspectRatioChange}>
+        {/* <div className="aspect-ratios" onChange={onAspectRatioChange}>
           <input type="radio" value={1 / 1} name="ratio" /> 1:1
           <input type="radio" value={5 / 4} name="ratio" /> 5:4
           <input type="radio" value={4 / 3} name="ratio" /> 4:3
@@ -45,7 +45,7 @@ function ImageCropper({ image, onCropDone, onCropCancel }) {
           <input type="radio" value={5 / 3} name="ratio" /> 5:3
           <input type="radio" value={16 / 9} name="ratio" /> 16:9
           <input type="radio" value={3 / 1} name="ratio" /> 3:1
-        </div>
+        </div> */}
 
         <button className="btn btn-outline" onClick={onCropCancel}>
           Cancel
